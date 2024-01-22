dd0af2bc220215762c931224c9e6f25b0d458978
yeeun000 <108613541+yeeun000@users.noreply.github.com>
2024-01-23 00:42:54 +0900

    test1

--- jaehyeon/codesnack/src/main/java/com/example/codesnack/posts/PostController.java
+++
@@ -1,8 +0,0 @@
-package com.example.codesnack.posts;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class PostController {
-
-}
--- jaehyeon/codesnack/src/main/java/com/example/codesnack/posts/PostService.java
+++
@@ -1,5 +0,0 @@
-package com.example.codesnack.posts;
-
-public interface PostService {
-
-}
