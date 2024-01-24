25798cb289fdcf9238508a594e0f0576f8aacda2
yeeun000 <108613541+yeeun000@users.noreply.github.com>
2024-01-24 21:43:19 +0900

    게시판

--- Web/src/main/java/com/example/WebProject/repository/FreeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.WebProject.repository;
-
-import com.example.WebProject.entity.Free;
-import org.springframework.data.repository.CrudRepository;
-
-public interface FreeRepository extends CrudRepository<Free, Long> {
-}
